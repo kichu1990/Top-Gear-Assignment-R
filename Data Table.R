eb37730a2421acac19cd52177255c8a8d11da27e
kichu1990 <34007976+kichu1990@users.noreply.github.com>
2018-07-06 17:14:46 +0530

    Update Data Table.R

--- Data Table.R
+++ Data Table.R
@@ -2,13 +2,11 @@ install.packages("data.table")
 
 #load required library
 library(data.table)
-
 mydata = read.csv("C:/Users/ABHIC6/Documents/flights_2014.csv")
 
 View(mydata)
 
 nrow(mydata)   #No of Rows
-
 ncol(mydata)   #No of Columns
 
 names(mydata)  #Return Columnnames
